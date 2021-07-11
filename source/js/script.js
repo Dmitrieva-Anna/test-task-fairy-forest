24238fa949fd28f93d6fd8cb2fb93a2b1d4bc41b
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2021-07-12 01:49:52 +0300

    burger menu
    upgrade media 767

--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,6 @@
-console.log('Example')
-;
+var btnMenu = document.querySelector(".header__burger");
+var nav = document.querySelector('#nav');
+
+btnMenu.addEventListener('click', function () {
+    nav.classList.toggle('move');
+});
