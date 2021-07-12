240965178abf62dd9ed3c2bce8397c7bf588f0f1
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2021-07-12 23:35:27 +0300

    Final
    done project

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,12 @@
 var btnMenu = document.querySelector(".header__burger");
 var nav = document.querySelector('#nav');
+var tur = document.querySelector(".header__view");
+var lock = document.querySelector('#lock');
+var metroAddress = document.querySelector('.location__metro-address');
 
 btnMenu.addEventListener('click', function () {
     nav.classList.toggle('move');
+    tur.classList.toggle('move');
+    btnMenu.classList.toggle('change');
+    lock.classList.toggle('lock');
 });
